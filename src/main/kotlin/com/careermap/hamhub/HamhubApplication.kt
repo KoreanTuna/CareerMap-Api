9e793a17e9103b62eec5a2ae577a7bb3bb358b0b
KoreanTuna <songsee0528@gmail.com>
2025-12-19 10:49:43 +0900

    project init

---
+++ src/main/kotlin/com/careermap/hamhub/HamhubApplication.kt
@@ -0,0 +1,11 @@
+package com.careermap.hamhub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HamhubApplication
+
+fun main(args: Array<String>) {
+	runApplication<HamhubApplication>(*args)
+}
---
+++ src/test/kotlin/com/careermap/hamhub/HamhubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.careermap.hamhub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HamhubApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
